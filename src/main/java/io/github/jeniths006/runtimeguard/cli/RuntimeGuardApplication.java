14fddfbde57395ff90d9f4f79b1eb203984f0607
jeniths006 <jeniths006@gmail.com>
2026-07-10 19:37:30 +0100

    feat(cli): add application entry point

---
+++ src/main/java/io/github/jeniths006/runtimeguard/cli/RuntimeGuardApplication.java
@@ -0,0 +1,9 @@
+package io.github.jeniths006.runtimeguard.cli;
+
+public class RuntimeGuardApplication {
+
+    public static void main(String[] args) {
+        System.out.println("RuntimeGuard starting...");
+
+    }
+}
