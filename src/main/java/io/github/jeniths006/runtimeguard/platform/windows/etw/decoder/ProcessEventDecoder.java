f18c8f4eaf5f6cf022f1efcdeb8501630f7749e9
jeniths006 <jeniths006@gmail.com>
2026-08-07 20:18:58 +0100

    feat(etw): update ProcessEvent to include imageName and enhance event handling

--- src/main/java/io/github/jeniths006/runtimeguard/platform/windows/etw/decoder/ProcessEventDecoder.java
+++ src/main/java/io/github/jeniths006/runtimeguard/platform/windows/etw/decoder/ProcessEventDecoder.java
@@ -11,8 +11,8 @@ public class ProcessEventDecoder {
         long pid = extractPid(record, opcode);
 
         return switch (opcode) {
-            case 2 -> new ProcessEvent(pid, ProcessEventType.START);
-            case 11 -> new ProcessEvent(pid, ProcessEventType.STOP);
+            case 2 -> new ProcessEvent(pid, extractImageName(record), ProcessEventType.START);
+            case 11 -> new ProcessEvent(pid, extractImageName(record), ProcessEventType.STOP);
             default -> null;
         };
     }
@@ -28,5 +28,25 @@ public class ProcessEventDecoder {
             case 11 -> Integer.toUnsignedLong(record.userData.getInt(0));
             default -> -1;
         };
+    }
+
+    private String extractImageName(EventRecord record) {
+
+        if(record.userData == null) {
+            return "unknown";
+        }
+
+        //Image offset at 0x5c
+        byte[] bytes = record.userData.getByteArray(0x5c, 256);
+
+        String value = new String(bytes, java.nio.charset.StandardCharsets.UTF_16LE);
+
+        int nullIndex = value.indexOf('\0');
+
+        if (nullIndex != 1) {
+            return value.substring(0, nullIndex);
+        }
+
+        return value;
     }
 }
